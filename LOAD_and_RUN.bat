83c7a88647cafc06dbac4e570866557e5c7a1a94
josep.quintana <josep.quintana.44@gmail.com>
2018-12-10 17:40:37 +0100

    Scripts Path Relatiu

--- LOAD_and_RUN.bat
+++ LOAD_and_RUN.bat
@@ -1,6 +1,29 @@
-## LOAD ##
+## LOAD & RUN ##
 (clear)
 (load "./ontology/Practica.pont")
 (load "./Practica.clp")
 (load "./ontology/Practica.pins")
 (reset)
+(run)
+Vinicius Jr
+98
+si
+9
+no
+si
+3
+6
+no
+3
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
